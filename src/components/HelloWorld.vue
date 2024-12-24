951b7bac836c39d176f3560b713ca55f3fd1ab13
Ahmed Dame Samb <asamb650@gmail.com>
2024-12-24 12:00:53 +0000

    change code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,10 +13,8 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+
+      <h>Ahmed Dame</h>
   </div>
 
   <p>
